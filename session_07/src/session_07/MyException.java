a153650d432db115e20961492abaa2871ad0648a
91996 <91996@Tanay>
2024-02-21 12:35:13 +0530

    Final comit

---
+++ session_07/src/session_07/MyException.java
@@ -0,0 +1,12 @@
+package session_07;
+
+public class MyException extends Exception
+{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "MyException Occured!!!";
+	}
+	
+}
